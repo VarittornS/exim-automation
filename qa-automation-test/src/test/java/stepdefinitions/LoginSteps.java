d992debf85181988808f10e48de577c1205544f6
Varittorn S <varittorn.s@email.com>
2026-03-02 18:23:37 +0700

    fix: update CI

--- qa-automation-test/src/test/java/stepdefinitions/LoginSteps.java
+++ qa-automation-test/src/test/java/stepdefinitions/LoginSteps.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.*;
 import org.junit.Assert;
 import java.time.Duration;
